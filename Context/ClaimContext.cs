6225eb6a70c3594d9904201cad2af662dc43e651
simennielsen <simen.nielsen@karsten-moholt.no>
2021-02-09 00:18:34 +0100

    ClaimContext added

---
+++ Context/ClaimContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using System;
+using IC3000.Models;
+//using Microsoft.Extensions.Configuration;
+//using System.IO;
+
+namespace IC3000.Context
+{
+    public class ClaimContext : DbContext
+    {
+        public DbSet<Claim> Claims { get; set; }
+        public ClaimContext(DbContextOptions<ClaimContext> options) : base(options)
+        { }
+
+    }
+}
