726e04a900d5549a8c4f23506b80373cda8418f9
simennielsen <simen.nielsen@karsten-moholt.no>
2021-02-09 00:20:53 +0100

    QueueService to send message to ServiceBus

---
+++ Service/QueueService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace IC3000.Service
+{
+    public class QueueService
+    {
+        private ServiceBusClient _client;
+        private protected string _queueName;
+        /// <summary>
+        /// Establish connection to Service bus.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public QueueService(string connectionString)
+        {
+            _client = new ServiceBusClient(connectionString);
+        }
+        /// <summary>
+        /// Send message to service bus queue.
+        /// </summary>
+        /// <param name="_message"></param>
+        /// <returns></returns>
+        public async Task SendMessageAsync(string _message)
+        {
+
+            ServiceBusSender sender = _client.CreateSender("Claim");
+
+            // create a message that we can send
+            ServiceBusMessage message = new ServiceBusMessage(_message);
+
+            // send the message
+            await sender.SendMessageAsync(message);
+            Console.WriteLine($"Sent a single message to the queue: {_queueName}");
+        }
+    }
+}
